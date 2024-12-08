27d41c7824dc4c537780e0dc4ff0829b6292f5bc
sunakan <sunakan03@gmail.com>
2024-12-08 22:01:58 +0900

    fix2

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
-set -x
+# set -x
 # -e: エラーが発生した時点でスクリプトを終了
 # -u: 未定義の変数を使用した場合にエラーを発生
 # -x: スクリプトの実行内容を表示(debugで利用)
@@ -13,10 +13,9 @@ readonly DATA_FILE='data.csv'
 # ${DATA_FILE}を持ってくる
 #
 git fetch origin ${TARGET_BRANCH}
-git branch -D ${TARGET_BRANCH} || echo ''
+git branch -D ${TARGET_BRANCH} || echo "${TARGET_BRANCH} ブランチはありませんでした"
 git branch ${TARGET_BRANCH} origin/${TARGET_BRANCH}
 git restore --source ${TARGET_BRANCH} -- ${DATA_FILE}
-echo '-------A'
 
 #
 # 処理 & dataを退避
@@ -24,34 +23,36 @@ echo '-------A'
 echo "a,b,c,$(date)" >> ${DATA_FILE}
 cat ${DATA_FILE} | tail -n 3 > tmp/${DATA_FILE}
 rm data.csv
-echo '-------B'
 
 #
 # dataを持ってくる
 #
 git switch ${TARGET_BRANCH}
 mv tmp/data.csv ./
-echo '-------C'
 
 #
-# Auto commit
+# git config
 #
 # 自動でcommitする時のuserとemail指定の参考
 # https://github.com/orgs/community/discussions/26560
 #
+DEFAULT_USER_NAME='github-actions[bot]'
+DEFAULT_USER_EMAIL='41898282+github-actions[bot]@users.noreply.github.com'
+USER_NAME=$(git config user.name)
+USER_EMAIL=$(git config user.email)
+git config user.name "${USER_NAME:-${DEFAULT_USER_NAME}}"
+git config user.email "${USER_EMAIL:-${DEFAULT_USER_EMAIL}}"
+
+#
+# Auto commit
+#
 git stage data.csv
-readonly DEFAULT_USER_NAME='github-actions[bot]'
-readonly DEFAULT_USER_EMAIL='41898282+github-actions[bot]@users.noreply.github.com'
-readonly USER_NAME=$(git config user.name)
-readonly USER_EMAIL=$(git config user.email)
-git commit --amend --author="${USER_NAME:-${DEFAULT_USER_NAME}} <${USER_EMAIL:-${DEFAULT_USER_EMAIL}}>" -m "Automated commit"
-echo '-------D'
+git commit --amend -m "Automated commit"
 
 #
 # Force push
 #
 git push origin ${TARGET_BRANCH} --force
-echo '-------E'
 
 #
 # 戻る
